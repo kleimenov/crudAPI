b0df429139bea3e886baa7140b6d7d68c027a353
Иван <kleimenov416@gmail.com>
2020-10-21 17:26:37 -0400

    add CRUD routes, tested all features and connection with database, all work well

--- psqlTest.js
+++ psqlTest.js
@@ -1,3 +1,5 @@
+const { request, response } = require('express');
+
 const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'deelooc21',
@@ -5,27 +7,82 @@ const pool = new Pool({
     database: 'testdb',
     port: '5432'
 })
+//I will make a bunch of psql queries
 
+//1. Lets get all cats names from database
 const getCats = (request, response) => {
-    pool.query('select * from cats', (err, res) => {
-        if (err) throw err;
-        response.status(200).json(res.rows)
-    })
+   pool.query('select * from cats', (err, res) => {
+       if (err) throw err;
+       response.status(200).json(res.rows)
+   })
 }
 
-const getHHCDname = (request, response) => {
-    pool.query('select * from cats where id=1', (err, res) => {
-        if (err) throw err;
-        response.status(200).json(res.rows)
-    })
+//2. Lets get specific cat data from database
+const getCatById = (request, response) => {
+  const id = parseInt(request.params.id);
+  pool.query('select * from cats where id=$1', [id], (err, res) => {
+     if (err) throw err;
+      response.status(200).json(res.rows)
+  })
+}
+
+//3. Lets add new cats into our database
+const addCats = (request, response) => {
+  const {catName, catAge} = request.body;
+  pool.query('INSERT INTO cats (catName, catAge) VALUES ($1, $2)', [catName, catAge], (err, res) => {
+      if (err) throw err;
+       response.status(200).json(res.rows)
+   });
 }
 
 
+//4. Lets update cat data that already exist 
+/*
+const updateCats = (request, response) => {
+    const id = parseInt(request.params.id);
+    const {catName, catAge} = request.body;
+    pool.query('UPDATE cats SET catName =$1, email =$2 WHERE id =$3',
+      [catName, catAge, id],
+      (err, res) => {
+          if (err) throw err;
+           response.status(200).send(`cat modified with ID: ${id}`)
+       });
+  }
+*/
+  const updateCats = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { catName, catAge } = request.body
+  
+    pool.query(
+      'UPDATE cats SET catName = $1, catAge = $2 WHERE id = $3',
+      [catName, catAge, id],
+      (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`Cat modified with ID: ${id}`)
+      }
+    )
+  }
+
+//5. Lets delete cat from database
+const deleteCat = (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query('DELETE FROM cats WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Cat deleted with ID: ${id}`)
+    })
+  }
 
 //here we will export modules
 module.exports = {
     getCats,
-    getHHCDname
+    getCatById,
+    addCats,
+    updateCats,
+    deleteCat
 }
 
 
--- server.js
+++ server.js
@@ -19,14 +19,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 //try to do plain response - request
 app.get('/', (req, res) => {
     res.json({message: 'Node.js, Express and Postgres inside one boat EEEeeeehaaaaAAAA'});
-})
+});
 
 //here we will get response from database to '/cats'
-app.get('/cats', db.getCats)
+app.get('/cats', db.getCats);
 
-//where we will get a single user data (for instance particular user)
-app.get('/cats/1', db.getHHCDname)
+//here we will get a single user data (for instance particular user)
+app.get('/cats/:id', db.getCatById);
 
+//here we will add new data to db
+app.post('/cats/', db.addCats);
+
+//here we will update specific cat data
+app.put('/cats/:id', db.updateCats);
+
+//here we will delete cat from database
+app.delete('/cats/:id', db.deleteCat);
 
 
 //set port and start listen requests 
